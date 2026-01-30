2867c9009c5b19123399ff35e11bf51f2fecf0cc
Rada <sirada_dee@cmu.ac.th>
2026-01-30 14:46:12 +0700

    lab15_5.cpp completed

--- lab15_5.cpp
+++ lab15_5.cpp
@@ -18,5 +18,16 @@ int main(){
 	return 0;
 }
 
-//Do not modify the code above this line
-//Write definition of shuffle() using pointer after this line. 
+void shuffle(int &a, int &b, int &c, int &d) {
+    int money[] = {a, b, c, d};
+    for (int i = 3; i > 0; i--) {
+        int j = rand() % (i + 1);
+        int temp = money[i];
+        money[i] = money[j];
+        money[j] = temp;
+    }
+    a = money[0];
+    b = money[1];
+    c = money[2];
+    d = money[3];
+}
