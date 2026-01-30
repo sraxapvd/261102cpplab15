f6d2d16c16f77f04a45d968e93ca5b3799f40ca9
Rada <sirada_dee@cmu.ac.th>
2026-01-30 14:43:09 +0700

    lab15_4.cpp completed

--- lab15_4.cpp
+++ lab15_4.cpp
@@ -18,4 +18,16 @@ int main(){
 	return 0;
 }
 
-//Do not modify source code above this line
+void shuffle(int &a, int &b, int &c, int &d) {
+    int money[] = {a, b, c, d};
+    for (int i = 3; i > 0; i--) {
+        int j = rand() % (i + 1);
+        int temp = money[i];
+        money[i] = money[j];
+        money[j] = temp;
+    }
+    a = money[0];
+    b = money[1];
+    c = money[2];
+    d = money[3];
+}
